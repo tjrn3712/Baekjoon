0bc4cb3be0fdbb1ad0043e4f5fb5922d29c768e4
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2024-10-30 23:09:47 +0900

    [Silver I] Title: 절댓값 힙, Time: 176 ms, Memory: 50580 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+import math
+import heapq
+import random
+ipt = sys.stdin.readline
+def minput(): return map(int, ipt().split())
+
+
+n = int(ipt())
+h = []
+for _ in range(n):
+    x = int(ipt())
+    if x == 0:
+        if h:
+            print(heapq.heappop(h)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(h, [abs(x), x])
