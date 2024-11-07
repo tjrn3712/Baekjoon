c1ab23a78fceca1356736fb865b072b62aec010f
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2024-11-07 18:36:18 +0900

    [Silver II] Title: On My Way Dorm, Time: 76 ms, Memory: 43724 KB -BaekjoonHub

---
+++ 백준/Silver/27868. On My Way Dorm/On My Way Dorm.py
@@ -0,0 +1,12 @@
+import sys
+from collections import deque
+import math
+import heapq
+import random
+ipt = sys.stdin.readline
+def minput(): return map(int, ipt().split())
+
+
+ipt()
+ipt()
+print(ipt().rstrip('\n')[::-1])
