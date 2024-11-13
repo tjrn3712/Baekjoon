83d5a3393aed13c7de156805a43bc9cac8f9dfee
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2024-11-13 23:39:42 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 348 ms, Memory: 141816 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+import math
+import heapq
+import random
+import itertools
+ipt = sys.stdin.readline
+def minput(): return map(int, ipt().split())
+
+
+def bfs(graph, i, visited):
+    q = deque()
+    t = 1
+    if not visited[i]:
+        visited[i] = True
+        nums[i] = t
+        t += 1
+        for j in sorted(graph[i]):
+            q.append(j)
+    while q:
+        temp = q.popleft()
+        if not visited[temp]:
+            nums[temp] = t
+            t += 1
+            visited[temp] = True
+            for j in sorted(graph[temp]):
+                q.append(j)
+
+
+n, m, r = minput()
+graph = [[] for i in range(n+1)]
+visited = [False] * (n+1)
+for _ in range(m):
+    u, v = minput()
+    graph[u].append(v)
+    graph[v].append(u)
+nums = [0]*(n+1)
+bfs(graph, r, visited)
+
+for i in range(1, n+1):
+    print(nums[i])
