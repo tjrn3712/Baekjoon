107e285e6a626502bd23230fd080668b0f5d8e13
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2024-11-19 01:08:09 +0900

    [Bronze V] Title: 오늘 날짜, Time: 128 ms, Memory: 112520 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+import math
+import heapq
+import random
+import itertools
+ipt = sys.stdin.readline
+def minput(): return map(int, ipt().split())
+mod = 10**9+7
+
+
+print('2024-11-19')
+
