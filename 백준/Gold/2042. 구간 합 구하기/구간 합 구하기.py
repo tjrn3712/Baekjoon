f6ec1cc306fa4b1a66c4a7923e34497a2179c61c
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2024-12-02 02:27:51 +0900

    [Gold I] Title: 구간 합 구하기, Time: 552 ms, Memory: 198388 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -3,43 +3,64 @@ from collections import deque
 import math
 import heapq
 import random
-ipt = sys.stdin.readline
-def minput(): return map(int, ipt().split())
-
-def seg_init(start,end,index):
-    if start == end:
-        tree[index] = arr[start]
-        return tree[index]
-    mid = (start+end) //2
-    tree[index] = seg_init(start, mid, index*2) + seg_init(mid+1, end, index*2+1)
-    return tree[index]
-def seg_update(start,end,index,ind_update,value):
-    if ind_update < start or ind_update > end:
-        return tree[index]
-    if start == end:
-        tree[index] = value
-        return tree[index]
-    mid = (start + end) // 2
-    tree[index] = seg_update(start, mid, index * 2, ind_update, value) + seg_update(mid + 1, end, index * 2 + 1, ind_update, value)
-    return tree[index]
-def seg_prefix(start,end,index,left,right):
-    if left > end or right < start:
-        return 0
-    if left <= start and right >= end:
-        return tree[index]
-    mid = (start+end) //2
-    return seg_prefix(start, mid, index*2, left, right)+seg_prefix(mid+1, end, index*2+1, left, right)
+import itertools
+input = sys.stdin.readline
+INF = float('inf')
+MOD = 998244353
+def minput(): return map(int, input().split())
 
 
-arr = []
+class SegmentTree:
+    # non-recursive
+    # by tjrn3712(2024.11.07)
+    def __init__(self, arr, merge, i):
+        self.i = i
+        self.N = len(arr)
+        self.tree = [i for _ in range(self.N << 1)]
+        self.merge = merge
+        for j in range(self.N):
+            self.tree[self.N+j] = arr[j]
+        for j in range(self.N-1, 0, -1):
+            self.tree[j] = merge(self.tree[j<<1], self.tree[j<<1|1])
+
+    def update_index(self, index, value):
+        index += self.N
+        self.tree[index] = value
+        while index > 1:
+            self.tree[index>>1] = self.merge(self.tree[index], self.tree[index^1])
+            index >>= 1
+
+    # [left, right)
+    # [left, right-1]
+    def query(self, left, right):
+        left += self.N
+        right += self.N
+        result = self.i
+        while left < right:
+            if left & 1:
+                result = self.merge(result, self.tree[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                result = self.merge(result, self.tree[right])
+            left >>= 1
+            right >>= 1
+        return result
+
+    def update_range(self, left, right, value):
+        pass
+        # studying
+        # lazy propagation(non-recursive)
+
+
 n, m, k = minput()
+arr = []
 for _ in range(n):
-    arr.append(int(ipt()))
-tree = [0]*2*2**math.ceil(math.log(n, 2))
-seg_init(0,n-1,1)
+    arr.append(int(input()))
+seg = SegmentTree(arr, lambda a, b: a+b, 0)
 for _ in range(m+k):
     a, b, c = minput()
     if a == 1:
-        seg_update(0,n-1,1,b-1,c)
+        seg.update_index(b-1, c)
     elif a == 2:
-        print(seg_prefix(0,n-1,1,b-1,c-1))
+        print(seg.query(b-1, c))
