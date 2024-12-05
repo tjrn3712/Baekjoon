9739306e35b059a7e81e0681567d94f7dd8e8c9b
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2024-12-05 22:50:26 +0900

    [Gold IV] Title: 두 배, Time: 232 ms, Memory: 150700 KB, Score: 33 point -BaekjoonHub

---
+++ 백준/Gold/31963. 두 배/두 배.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+import math
+import heapq
+import random
+import itertools
+input = sys.stdin.readline
+INF = float('inf')
+MOD = 10**9+7
+def minput(): return map(int, input().split())
+
+
+n = int(input())
+a = [*minput()]
+cnt = 0
+for i in range(n-1):
+    if a[i] > a[i+1]:
+        cnt += math.ceil(math.log(a[i] / a[i + 1], 2))
+        a[i+1] *= pow(2, math.ceil(math.log(a[i]/a[i+1], 2)))
+print(cnt)
