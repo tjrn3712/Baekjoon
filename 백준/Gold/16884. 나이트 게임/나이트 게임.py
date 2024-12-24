15e19f21e2e0d59fc1d4f1947d5655239d01ef69
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2024-12-24 16:13:53 +0900

    [Gold III] Title: 나이트 게임, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/16884. 나이트 게임/나이트 게임.py
+++ 백준/Gold/16884. 나이트 게임/나이트 게임.py
@@ -1,17 +1,3 @@
-import sys
-from collections import deque
-import math
-import heapq
-import random
-ipt = sys.stdin.readline
-sys.setrecursionlimit(10**9)
-def minput(): return map(int, ipt().split())
-
-
-t = int(ipt())
-for _ in range(t):
-    n = int(ipt())
-    if n%2:
-        print('koosaga')
-    else:
-        print('cubelover')
+for _ in range(int(input())):
+    if int(input())&1:print('koosaga')
+    else:print('cubelover')
