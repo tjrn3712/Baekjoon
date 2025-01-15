20f5bd99d3c6bf7df7d2a82f08e10c711e7bf60e
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-01-15 22:29:00 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a=1342,b=23652453;
+    while(a+b){
+        cin>>a>>b;
+        if(a+b)cout<<a+b<<"\n";
+    }
+}
