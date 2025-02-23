8b34326615b3d5b46a715fa2d3ea7564322975fb
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-02-23 22:50:21 +0900

    [Gold V] Title: 택배 배송, Time: 260 ms, Memory: 126640 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -0,0 +1,28 @@
+import sys, heapq
+input = sys.stdin.readline
+def minput(): return map(int, input().split())
+
+
+def dijkstra(graph, i, distance):
+    pq = []
+    distance[i] = 0
+    heapq.heappush(pq, (distance[i], i))
+    while pq:
+        dist_now, current = heapq.heappop(pq)
+        if distance[current] < dist_now:
+            continue
+        for j in graph[current]:
+            if dist_now + j[1] < distance[j[0]]:
+                distance[j[0]] = dist_now + j[1]
+                heapq.heappush(pq, (distance[j[0]], j[0]))
+
+
+n, m = minput()
+graph = [[]for i in range(n+1)]
+dist = [998244353]*(n+1)
+for _ in range(m):
+    a,b,c = minput()
+    graph[a].append([b,c])
+    graph[b].append([a,c])
+dijkstra(graph, 1, dist)
+print(dist[n])
