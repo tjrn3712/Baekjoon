40b79a1b6558b69286cb68bef9913e0901c0a7b5
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-03-03 00:26:45 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 4188 ms, Memory: 275716 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+def minput(): return map(int, input().split())
+
+
+def bipartite_matching(adjacent, graph1, graph2, a, visited):
+    visited[a] = True
+    for b in adjacent[a]:
+        if graph2[b] == -1 or not visited[graph2[b]] and bipartite_matching(adjacent, graph1, graph2, graph2[b], visited):
+            graph1[a] = b
+            graph2[b] = a
+            return True
+    return False
+
+
+n, m = minput()
+adj = [[] for i in range(max(n,m)+1)]
+for i in range(1, n+1):
+    adj[i] = [*minput()][1:]
+ans = 0
+A = [-1]*(n+1)
+B = [-1]*(m+1)
+for i in range(1, n+1):
+    visited = [False]*(n+1)
+    if bipartite_matching(adj, A, B, i, visited): ans += 1
+ans2 = 0
+for i in range(1, n+1):
+    visited = [False]*(n+1)
+    if bipartite_matching(adj, A, B, i, visited): ans2 += 1
+print(ans+ans2)
