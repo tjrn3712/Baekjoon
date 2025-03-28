84565cba39d601f29951a64a5ec4939f3860cc83
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-03-28 13:22:25 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1036 ms, Memory: 17524 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,3 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
-main(){ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);long long n,m,k,f,b,c;cin>>n>>m>>k;long long r,a[n],s[n<<1]={0,};for(int i=0;i<n;i++)cin>>a[i];for(int i=0;i<n;i++)s[n+i]=a[i];for(int i=n-1;i>0;i--)s[i]=s[i<<1]+s[i<<1|1];for(int i=0;i<m+k;i++){cin>>f;if(--f){cin>>b>>c;r=0;for(b+=n-1,c+=n;b<c;b>>=1,c>>=1){if(b&1)r=r+s[b++];if(c&1) r=r+s[--c];}cout<<r<<'\n';}else{cin>>b>>c;for(s[b=--b+n]=c;b>1;b>>=1)s[b>>1]=s[b]+s[b^1];}}}
+#include <iostream>
+using namespace std;main(){int n,m,k,i=0,f;cin>>n>>m>>k;long long r,s[n*2]={},b,c;for(;i<n;i++)cin>>s[n+i];for(i--;i>0;i--)s[i]=s[i*2]+s[i*2+1];for(;i<m+k;i++){cin>>f>>b>>c;if(--f){r=0;for(b+=n-1,c+=n;b<c;b/=2,c/=2){if(b&1)r+=s[b++];if(c&1)r+=s[--c];}cout<<r<<'\n';}else for(s[b=--b+n]=c;b>1;b/=2)s[b/2]=s[b]+s[b^1];}}
