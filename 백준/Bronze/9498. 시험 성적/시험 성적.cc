c42377028519d71488fca6d84356912326e199cc
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-06-30 23:49:48 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.cc
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -5,11 +5,8 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n;
-    cin >> n;
-    if (n>=90) cout << "A";
-    else if (n>=80) cout << "B";
-    else if (n>=70) cout << "C";
-    else if (n>=60) cout << "D";
-    else cout << "F";
+
+    int a;
+    cin>>a;
+    cout<<(a>89?"A":a>79?"B":a>69?"C":a>59?"D":"F");
 }
