1dbfc1e4365369c14f6d9961c8f84f5e3873ba77
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-06-30 23:28:11 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,11 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int a, b;
-    cin >> a >> b;
-    cout << a+b;
+
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b;
 }
