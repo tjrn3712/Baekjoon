9f3e5e3cea3f313f738d0fb4309620316231178d
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-06-30 23:28:41 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.cc
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -5,7 +5,8 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int a, b;
-    cin >> a >> b;
-    cout << a-b;
+
+    int a,b;
+    cin>>a>>b;
+    cout<<a-b;
 }
