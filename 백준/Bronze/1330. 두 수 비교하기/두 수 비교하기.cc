1f186fa0b6e91e38b2ed825a2920d97a9e2983f3
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-06-30 23:48:32 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -5,13 +5,8 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int a, b;
-    cin >> a >> b;
-    if(a>b){
-        cout << ">";
-    }else if(a<b){
-        cout << "<";
-    }else{
-        cout << "==";
-    }
+
+    int a,b;
+    cin>>a>>b;
+    cout<<(a==b?"==":a<b?"<":">");
 }
