3251c37aebc16eb8e5ac74d88434e4ec7f558069
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-06-30 23:39:35 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.cc
+++ 백준/Bronze/10172. 개/개.cc
@@ -5,5 +5,6 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cout << "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|";
+
+    cout<<"|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|";
 }
