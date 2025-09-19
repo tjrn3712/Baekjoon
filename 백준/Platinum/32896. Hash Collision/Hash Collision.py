adcd40d8f0972f91f30d5bc199a76040a6567eec
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-09-19 13:47:14 +0900

    [Platinum III] Title: Hash Collision, Time: 152 ms, Memory: 38112 KB -BaekjoonHub

--- 백준/Platinum/32896. Hash Collision/Hash Collision.py
+++ 백준/Platinum/32896. Hash Collision/Hash Collision.py
@@ -1,10 +1,7 @@
-i=input
+i=lambda:int(input())
 p=print
-t=int
-n=t(i())
+n=i()
 p('?',n,1)
-c=t(i())
-if c==n:exit(p('!',c,1))
-p('?',n-c,1)
-r=t(i())
-p('!',c,r)
+c=i()
+if c^n:p('?',n-c,1);p('!',c,i())
+p('!',c,1)
