746a8ab3d9c583449f2a6f499d18e049b8367e50
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-09-25 18:40:31 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1024 ms, Memory: 119544 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,3 +1,3 @@
-#include<iostream>
+#import<iostream>
 #define _ std::cin
-long n,i,s['力'],b,c;main(){for(_>>n>>b>>c;i<n;_>>s[n+i++]);for(;--i;s[i]=s[i*2]+s[i*2+1]);for(;_>>i>>b>>c;){b+=~-n;if(~----i){for(c+=n;b<c;b/=2,c/=2)i+=(b&1?s[b++]:0)+(c&1?s[--c]:0);std::cout<<i<<' ';}else for(s[b]=c;b>1;s[b/=2]=s[b]+s[b^1]);}}
+long n,i,s['力'],b,c;main(){for(_>>n>>b>>c;i<n;_>>s[n+i++]);for(;--i;s[i]=s[i*2]+s[i*2+1]);for(;_>>i>>b>>c;)if(b+=~-n,~----i){for(c+=n;b<c;b/=2,c/=2)i+=b&1?s[b++]:0,i+=c&1?s[--c]:0;std::cout<<i<<' ';}else for(c-=s[b];s[b]+=c,b/=2;);}
