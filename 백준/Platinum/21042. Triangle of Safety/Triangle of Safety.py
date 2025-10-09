e9ab636f9a632be6b2eb671ff58c259eb5411f09
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-10-10 01:19:53 +0900

    [Platinum V] Title: Triangle of Safety, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Platinum/21042. Triangle of Safety/Triangle of Safety.py
+++ 백준/Platinum/21042. Triangle of Safety/Triangle of Safety.py
@@ -1,2 +1,1 @@
-for i in range(25):
- for x,y in[(1,12),(2,8),(3,10),(4,9)]:print(chr(i+65)+chr(65+(i+x)%25)+chr(65+(i+y)%25))
+for i in range(25):[print(chr(i+65)+chr(65+(i+x)%25)+chr(65+(i+y)%25))for x,y in[(1,12),(2,8),(3,10),(4,9)]]
