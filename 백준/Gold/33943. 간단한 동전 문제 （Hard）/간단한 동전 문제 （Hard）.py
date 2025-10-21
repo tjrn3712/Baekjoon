5ce90c2ec0a79db36322a7ab313d53625af0d471
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-10-22 08:06:45 +0900

    [Gold IV] Title: 간단한 동전 문제 (Hard), Time: 468 ms, Memory: 112140 KB -BaekjoonHub

---
+++ 백준/Gold/33943. 간단한 동전 문제 （Hard）/간단한 동전 문제 （Hard）.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+def minput(): return map(int, input().split())
+
+n, m = minput()
+if m==0: exit(print(0))
+if n==0: exit(print(-1))
+p = [*minput()]
+q = deque()
+q.append((10000,0))
+coin = [0]*20001
+while q:
+    t, cnt = q.popleft()
+    for i in p:
+        if -1<t+i<20001 and not coin[t+i]:
+            if t+i==m+10000: exit(print(cnt+1))
+            coin[t+i] = 1
+            q.append((t+i,cnt+1))
+print(-1)
