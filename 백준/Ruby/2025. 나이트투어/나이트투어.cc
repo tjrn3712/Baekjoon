940567e9843cee9763702455ff3187eb9b12dc53
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-11-20 16:48:48 +0900

    [Ruby V] Title: 나이트투어, Time: 128 ms, Memory: 8384 KB, Score: 97 point -BaekjoonHub

---
+++ 백준/Ruby/2025. 나이트투어/나이트투어.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[8]={2,1,-1,-2,-2,-1,1,2};
+int dy[8]={1,2,2,1,-1,-2,-2,-1};
+
+struct pos{int x,y;};
+int n,x,y,best,now,tx,ty,nx,ny,m;
+int possibleMove (vector<vector<bool>> &visited, int x, int y) {
+    int cnt=0,nx,ny;
+    for (int i=0;i<8;i++) {
+        nx=x+dx[i],ny=y+dy[i];
+        if (nx>0&&nx<=n&&ny>0&&ny<=n&&!visited[nx][ny]) cnt++;
+    }
+    return cnt;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>n>>x>>y;
+    vector<vector<bool>> visited(n+1,vector<bool>(n+1,0));
+    vector<pos> ans;
+
+    visited[x][y]=1;
+    ans.push_back({x,y});
+    for (int i=0;i<n*n-1;i++) {
+        best=9,tx=-1,ty=-1;
+        vector<pos> c;
+        for (int j=0;j<8;j++) {
+            nx=x+dx[j],ny=y+dy[j];
+            if (!(nx>0&&nx<=n&&ny>0&&ny<=n)||visited[nx][ny]) continue;
+            m=possibleMove(visited,nx,ny);
+            if (m<best) best=m,tx=nx,ty=ny;
+            if (m==best) c.push_back({nx,ny});
+        }
+        if (tx==-1) {
+            cout<<"-1 -1";
+            return 0;
+        }
+
+        best=998244353;
+        for (pos p:c) {
+            now=min(abs(1-p.x),abs(n-p.x))+min(abs(1-p.y),abs(n-p.y));
+            if (now<best) tx=p.x,ty=p.y;
+        }
+        x=tx,y=ty;
+        visited[x][y]=1;
+        ans.push_back({x,y});
+    }
+
+    for (pos i:ans) cout<<i.x<<' '<<i.y<<'\n';
+}
