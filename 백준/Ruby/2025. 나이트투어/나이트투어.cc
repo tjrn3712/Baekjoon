9ef08a8456654b265992b1689f1ffaca014277f5
tjrn3712 <86279370+tjrn3712@users.noreply.github.com>
2025-11-20 17:14:08 +0900

    [Ruby V] Title: 나이트투어, Time: 308 ms, Memory: 8384 KB, Score: 253 point -BaekjoonHub

--- 백준/Ruby/2025. 나이트투어/나이트투어.cc
+++ 백준/Ruby/2025. 나이트투어/나이트투어.cc
@@ -18,8 +18,10 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    bool ok=1;
 
     cin>>n>>x>>y;
+    int ox=x,oy=y;
     vector<vector<bool>> visited(n+1,vector<bool>(n+1,0));
     vector<pos> ans;
 
@@ -41,8 +43,8 @@ int main(){
             if (m==best) c.push_back({nx,ny});
         }
         if (tx==-1) {
-            cout<<"-1 -1";
-            return 0;
+            ok=0;
+            break;
         }
 
         best=998244353;
@@ -54,6 +56,48 @@ int main(){
         visited[x][y]=1;
         ans.push_back({x,y});
     }
+    if (!ok) {
+        ok=1;
+        x=ox,y=oy;
+        vector<vector<bool>> visited(n+1,vector<bool>(n+1,0));
+        ans.clear();
 
+        visited[x][y]=1;
+        ans.push_back({x,y});
+
+        for (int i=0;i<n*n-1;i++) {
+            best=9,tx=-1,ty=-1;
+            vector<pos> c;
+            for (int j=0;j<8;j++) {
+                nx=x+dx[j],ny=y+dy[j];
+                if (!(nx>0&&nx<=n&&ny>0&&ny<=n)||visited[nx][ny]) continue;
+                m=possibleMove(visited,nx,ny);
+                if (m<best) best=m,tx=nx,ty=ny;
+            }
+            for (int j=0;j<8;j++) {
+                nx=x+dx[j],ny=y+dy[j];
+                if (!(nx>0&&nx<=n&&ny>0&&ny<=n)||visited[nx][ny]) continue;
+                m=possibleMove(visited,nx,ny);
+                if (m==best) c.push_back({nx,ny});
+            }
+            if (tx==-1) {
+                ok=0;
+                break;
+            }
+
+            best=998244353;
+            for (pos p:c) {
+                now=min(min(abs(1-p.x),abs(n-p.x)),min(abs(1-p.y),abs(n-p.y)));
+                if (now<best) best=now,tx=p.x,ty=p.y;
+            }
+            x=tx,y=ty;
+            visited[x][y]=1;
+            ans.push_back({x,y});
+        }
+    }
+    if (!ok) {
+        cout<<"-1 -1";
+        return 0;
+    }
     for (pos i:ans) cout<<i.x<<' '<<i.y<<'\n';
 }
